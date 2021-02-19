87300766b3577d3e84c9b1fa113dd4ff31308456
Claudio Rivas Marín <58704834+rm-claudio22@users.noreply.github.com>
2021-02-19 17:22:28 -0500

    Update settings.py

--- FUNDASTORE/settings.py
+++ FUNDASTORE/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dev.clrivasmarin@gmail.com'
-EMAIL_HOST_PASSWORD = 'xxxxxxxxxxxxxxxxxxxx'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 
 import dj_database_url
